f15d1c20774d4b4746e10b904adb9feff91f1551
jjunseoPark <pycyej12@gmail.com>
2024-08-12 18:49:09 +0900

    07261745

---
+++ lib/home/food_information_parts/food_topbar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class FoodTopbar extends StatefulWidget {
+  const FoodTopbar({super.key});
+
+  @override
+  State<FoodTopbar> createState() => _FoodTopbarState();
+}
+
+class _FoodTopbarState extends State<FoodTopbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+
+      ],
+    );
+  }
+}
